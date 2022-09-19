d6990451cdba22f1ac6b6741e797783350df655a
Abhishek73025 <abhi.ag.kas@gmal.com>
2022-09-19 09:02:13 +0530

    part-6

--- frontend/src/components/Profie.js
+++ frontend/src/components/Profie.js
@@ -1,5 +1,63 @@
 import React from "react";
+import "./Profile.css";
 
 export default function Profie() {
-  return <div>Profie</div>;
+  return (
+    <div className="profile">
+      {/* Profile frame */}
+      <div className="profile-frame">
+        {/* profile-pic */}
+        <div className="profile-pic">
+          <img
+            src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        {/* profile-data */}
+        <div className="pofile-data">
+          <h1>Canta Coder</h1>
+          <div className="profile-info" style={{ display: "flex" }}>
+            <p>40 posts</p>
+            <p>40 followers</p>
+            <p>40 following</p>
+          </div>
+        </div>
+      </div>
+      <hr
+        style={{
+          width: "90%",
+
+          opacity: "0.8",
+          margin: "25px auto",
+        }}
+      />
+      {/* Gallery */}
+      <div className="gallery">
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
